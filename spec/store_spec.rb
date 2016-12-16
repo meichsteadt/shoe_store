39b9b3806f3d4442d7574411d1c68c6c4fd7c080
Matt Eichsteadt <matteichsteadt@yahoo.com>
2016-12-15 21:34:18 -0800

    add validation for Store

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe Store do
   it { should have_and_belong_to_many(:shoes) }
+  it("validates presence of name") do
+    store = Store.create({name: ""})
+    expect(store.save()).to(eq(false))
+  end
 end
