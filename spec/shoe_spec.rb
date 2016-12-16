f117863f2b0d8a6ba33ce454ad1e961e70f627f5
Matt Eichsteadt <matteichsteadt@yahoo.com>
2016-12-15 21:28:08 -0800

    remove test for create and read function

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,8 +2,4 @@ require 'spec_helper'
 
 describe Shoe do
   it { should have_and_belong_to_many(:stores) }
-  it "creates a Shoe brand" do
-    new_shoe = Shoe.create(name: 'Nike')
-    expect(Shoe.all).to eq [new_shoe]
-  end
 end
