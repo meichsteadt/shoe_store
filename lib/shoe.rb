173bdc260e3570aa12ae405597dac957abcc799b
Matt Eichsteadt <matteichsteadt@yahoo.com>
2016-12-15 21:45:02 -0800

    add capitalize to Shoe

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,10 @@
 class Shoe < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   validates(:name, :presence => true)
+  before_save :capitalize
+
+private
+  def capitalize
+    self.name = name.downcase.capitalize
+  end
 end
--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe Shoe do
   it { should have_and_belong_to_many(:stores) }
-  it("validates presence of name") do
+  it "validates presence of name" do
     shoe = Shoe.create({name: ""})
     expect(shoe.save()).to(eq(false))
+  end
+  it "converts the name to capitalized" do
+    shoe = Shoe.create(name: 'nIKE')
+    expect(shoe.name).to eq('Nike')
   end
 end
